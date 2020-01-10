53b917d27f762ab44026da5880b5d7b6634e7500
v-karapa <57983993+v-karapa@users.noreply.github.com>
2020-01-10 16:55:18 +0530

    updated

--- canvas_fileupload.ps1
+++ canvas_fileupload.ps1
@@ -1,7 +1,8 @@
 ﻿##########Canfile Upload##########
 #check configuration file is available or not
 
-if (-not (test-path conf.json)){
+if (-not (test-path conf.json))
+{
 
 #connect AzureAD
 Connect-AzureAD
@@ -10,7 +11,8 @@ Connect-AzureAD
 param(
       [Parameter(Mandatory=$true)][System.String]$appName,
       [Parameter(Mandatory=$true)][System.String]$Username,
-      [Parameter(Mandatory=$true)][System.String]$Password
+      [Parameter(Mandatory=$true)][System.String]$Password,
+      [Parameter(Mandatory=$true)][System.String]$New_synchronization_profile_Name
       )      
         $appHomePageUrl = "http://sissync.microsoft.com"
         $appURI = "http://sissync.microsoft.com/" + "$appName"
@@ -27,6 +29,16 @@ $ObjectId = (Get-AzureADApplication -Filter "DisplayName eq '$($appName)'" | sel
 $resource = "https://graph.microsoft.com/"
 $tenant = Get-AzureADTenantDetail
 $tenantid = $tenant.ObjectId
+$Domain = $tenant.VerifiedDomains
+
+#Getting SKuid
+$skuid = Get-AzureADSubscribedSku | select 
+$studentskuIds = $skuid | where {($_.skuPartNumber -eq 'M365EDU_A5_STUDENT')}
+$studentskuIds.skuId
+$teacherskuIds = $skuid | where {($_.skuPartNumber -eq 'M365EDU_A5_FACULTY')}
+$teacherskuIds.skuId
+$studentlicense = $studentskuIds.skuId
+$teacherlicense = $teacherskuIds.skuId
 
 #creating client secret
 $startDate = Get-Date
@@ -34,12 +46,16 @@ $endDate = $startDate.AddYears(3)
 $clientSecret = New-AzureADApplicationPasswordCredential -ObjectId $ObjectId -CustomKeyIdentifier "Secret01" -StartDate $startDate -EndDate $endDate
 $Client_Secret = $clientSecret.Value
         
-    $conf = [ordered]@{    
+    $conf = [ordered]@{
+    SyncprofileName= $SyncprofileName    
     client_Id     = $client_Id
     Client_Secret = $Client_Secret
     Username      = $Username
     Password      = $Password
     Tenantid      = $tenantid
+    Domain        = $Domain
+    Teacherlicense= $teacherlicense
+    Studentlicense= $studentlicense
     }
 
 $conf | ConvertTo-Json | Out-File -FilePath conf.json
@@ -52,97 +68,73 @@ else
   }
 
 ##Token generation
-    $confclient_Id     = $conffile.client_Id
-    $confClient_Secret = $conffile.Client_Secret
-    $confUsername      = $conffile.Username
-    $confPassword      = $conffile.Password
-    $conftenantid      = $conffile.Tenantid
-    $loginurl = "https://login.microsoftonline.com/" + "$conftenantid" + "/oauth2/v2.0/token"
+    $SyncprofileName= $conffile.SyncprofileName
+    $client_Id     = $conffile.client_Id
+    $Client_Secret = $conffile.Client_Secret
+    $Username      = $conffile.Username
+    $Password      = $conffile.Password
+    $tenantid      = $conffile.Tenantid
+    $Domain        = $conffile.Domain
+    $teacherlicense    = $conffile.Teacherlicense
+    $studentlicense    = $conffile.Studentlicense
+    
+    $loginurl = "https://login.microsoftonline.com/" + "$tenantid" + "/oauth2/v2.0/token"
 
     $ReqTokenBody = @{
      Grant_Type    = "Password"
-    client_Id     = $confclient_Id
-    Client_Secret = $confClient_Secret
-    Username      = $confUsername
-    Password      = $confPassword
+    client_Id     = $client_Id
+    Client_Secret = $Client_Secret
+    Username      = $Username
+    Password      = $Password
     Scope         = "https://graph.microsoft.com/.default"
 } 
 
-$TokenResponse = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody
+$Token = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody
 
 
 # Create header
 $Header = @{
     Authorization = "$($token.token_type) $($token.access_token)"
 }
-#get all syncronization profiles
-$Uri = "https://graph.microsoft.com/beta/education/synchronizationProfiles"
-$SyncProfiles = Invoke-RestMethod -Uri $Uri -Headers $Header -Method Get -ContentType "application/json"
-$SyncProfiles.Value
 
 #####create synchronization profiles####
-#Getting SKuid
-
-$Uri12 = "https://graph.microsoft.com/v1.0/subscribedSkus"
-$start1 = Invoke-RestMethod -Uri $Uri12 -Headers $Header -Method get -ContentType "application/json"
-$skuid = $start1.value | select skuId, skupartNumber
-
-$studentskuIds = $skuid | where {($_.skuPartNumber -eq 'M365EDU_A5_STUDENT')}
-$studentskuIds.skuId
-$teacherskuIds = $skuid | where {($_.skuPartNumber -eq 'M365EDU_A5_FACULTY')}
-$teacherskuIds.skuId
-
-
-$studomain = "M365EDU032767.onmicrosoft.com"
-$teacherdomain = "M365EDU032767.onmicrosoft.com"
-$studentlicense = $studentskuIds.skuId
-$teacherlicense = $teacherskuIds.skuId
 
 $body = 
 {
-    displayName = 'NewProfile12',
-    dataProvider = {
-        "@odata.type" : '#Microsoft.Education.DataSync.educationCsvDataProvider',
-        "customizations": {
-            "student": {
-                "optionalPropertiesToSync": [
-                    "State ID",
-                    "Middle Name"
-                ]
-            }
+    displayName = $New_synchronization_profile_Name
+    dataProvider = 
+        @{(odata.type) = "#Microsoft.Education.DataSync.educationCsvDataProvider"
+        customizations = {
+            student = {optionalPropertiesToSync = ("State ID", "Middle Name")}
         }
-    },
-    "identitySynchronizationConfiguration": {
-        "@odata.type" : "#Microsoft.Education.DataSync.educationIdentityCreationConfiguration",
-        "userDomains": [
+    }
+    identitySynchronizationConfiguration = 
+        @{(odata.type) = "#Microsoft.Education.DataSync.educationIdentityCreationConfiguration"
+        userDomains = {
             {
-                "appliesTo": "student",
-                "name": "M365EDU032767.onmicrosoft.com"
+                appliesTo = "teacher",
+                name = $Domain
             },
             {
-                "appliesTo": "teacher",
-                "name": "M365EDU032767.onmicrosoft.com"
+                appliesTo = "teacher",
+                name = $Domain
             }
-        ]
-    },
-    "licensesToAssign": [
-        {
-            "appliesTo": "teacher",
-            "skuIds": [
-                "e97c048c-37a4-45fb-ab50-922fbf07a370"
-            ]
-        },
-        {
-            "appliesTo": "student",
-            "skuIds": [
-                "46c119d4-0379-4a9d-85e4-97c66d3f909e"
-            ]
         }
-    ]
+    }
+    licensesToAssign = {
+        {
+            appliesTo = "teacher",
+            skuIds = $teacherlicense
+             },
+        {
+            appliesTo = "student",
+            skuIds = $studentlicense
+            }
+    }
 }
 $createdprofile = Invoke-RestMethod -Headers $Header -Uri $Uri -Body $body -Method Post -ContentType 'application/json'
 
-#upload url
+#create upload url
 $Uri1 = "https://graph.microsoft.com/beta/education/synchronizationProfiles/f7ae02dd-de69-415c-bf24-9b8f22af6d9b/uploadUrl"
 $uploadurl = Invoke-RestMethod -Uri $Uri1 -Headers $Header -Method Get -ContentType "application/json"
 
@@ -153,7 +145,7 @@ $c = ' --recursive=true --check-length=false'
 $u = "$a" + "'$b'" + "$c"
 $u >sastoken.cmd
 
-#run azcopy file #upload files using azcopy
+#run azcopy file and upload files using azcopy
 start-process -FilePath sastoken.cmd
 
 #Run start sync profile
