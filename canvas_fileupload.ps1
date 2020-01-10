c9c4519646d4f62c39f348d1eed2c34167f94af3
v-karapa <57983993+v-karapa@users.noreply.github.com>
2020-01-10 15:12:17 +0530

    added canvas file upload

--- canvas_fileupload.ps1
+++ canvas_fileupload.ps1
@@ -1,69 +1,108 @@
-﻿#connect AzureAD
+﻿##########Canfile Upload##########
+#check configuration file is available or not
+
+if (-not (test-path conf.json)){
+
+#connect AzureAD
 Connect-AzureAD
 
 #create Azure application
-$appName = "MSprofilesds"
-$appURI = "http://sissync.microsoft.com/sdsappconnect"
-$appHomePageUrl = "http://sissync.microsoft.com"
-$appReplyURLs = "https://localhost:1234"
+param(
+      [Parameter(Mandatory=$true)][System.String]$appName,
+      [Parameter(Mandatory=$true)][System.String]$Username,
+      [Parameter(Mandatory=$true)][System.String]$Password
+      )      
+        $appHomePageUrl = "http://sissync.microsoft.com"
+        $appURI = "http://sissync.microsoft.com/" + "$appName"
+        $appReplyURLs = "https://localhost:1234"
 
 if(!($myApp = Get-AzureADApplication -Filter "DisplayName eq '$($appName)'"  -ErrorAction SilentlyContinue))
 {
     $myApp = New-AzureADApplication -DisplayName $appName -IdentifierUris $appURI -Homepage $appHomePageUrl -ReplyUrls $appReplyURLs    
 }
 
-# Application (client) ID, tenant Name and secret
-$clientId = (Get-AzureADApplication -Filter "DisplayName eq '$($appName)'" | select AppId).AppId
+# Application (client) ID, tenant Name
+$client_Id = (Get-AzureADApplication -Filter "DisplayName eq '$($appName)'" | select AppId).AppId
 $ObjectId = (Get-AzureADApplication -Filter "DisplayName eq '$($appName)'" | select ObjectId).ObjectId
-$tenantName = "9bb0d80d-8b9d-4f06-a1fe-ef5e06c8a537"
 $resource = "https://graph.microsoft.com/"
+$tenant = Get-AzureADTenantDetail
+$tenantid = $tenant.ObjectId
 
 #creating client secret
 $startDate = Get-Date
 $endDate = $startDate.AddYears(3)
-$clientSecret = New-AzureADApplicationPasswordCredential -ObjectId $ObjectId -CustomKeyIdentifier "Secret05" -StartDate $startDate -EndDate $endDate
-
-
-#apply permistions
-$svcprincipalGraph = Get-AzureADServicePrincipal -All $true | ? { $_.DisplayName -match "Microsoft Graph" }
-
-$svcprincipalGraph.AppRoles | FT ID, DisplayName
-$svcprincipalGraph.Oauth2Permissions | FT ID, UserConsentDisplayName
-
-$Pbi = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
-$Pbi.ResourceAppId = $svcprincipalPbi.AppId
-
-$delPermission13 = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList "b2f1b2fa-f35c-407c-979c-a858a808ba85","Scope" ## View all workspaces
-#
-#authenticate application
-$Username = "admin@M365EDU032767.onmicrosoft.com"
-$Password = "8QTTmXqnbS"
-
-$ReqTokenBody = @{
-     Grant_Type    = "Password"
-    client_Id     = '61ef1a07-5f64-44c5-ad83-d3dc9713f989'
-    Client_Secret = 'z@AdIRu-D]fBK6?ixlkiAgt5hlQzxx53'
+$clientSecret = New-AzureADApplicationPasswordCredential -ObjectId $ObjectId -CustomKeyIdentifier "Secret01" -StartDate $startDate -EndDate $endDate
+$Client_Secret = $clientSecret.Value
+        
+    $conf = [ordered]@{    
+    client_Id     = $client_Id
+    Client_Secret = $Client_Secret
     Username      = $Username
     Password      = $Password
+    Tenantid      = $tenantid
+    }
+
+$conf | ConvertTo-Json | Out-File -FilePath conf.json
+
+}
+
+else
+  {
+    $conffile = get-content conf.json | ConvertFrom-Json
+  }
+
+##Token generation
+    $confclient_Id     = $conffile.client_Id
+    $confClient_Secret = $conffile.Client_Secret
+    $confUsername      = $conffile.Username
+    $confPassword      = $conffile.Password
+    $conftenantid      = $conffile.Tenantid
+    $loginurl = "https://login.microsoftonline.com/" + "$conftenantid" + "/oauth2/v2.0/token"
+
+    $ReqTokenBody = @{
+     Grant_Type    = "Password"
+    client_Id     = $confclient_Id
+    Client_Secret = $confClient_Secret
+    Username      = $confUsername
+    Password      = $confPassword
     Scope         = "https://graph.microsoft.com/.default"
 } 
-$TokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/9bb0d80d-8b9d-4f06-a1fe-ef5e06c8a537/oauth2/v2.0/token" -Method POST -Body $ReqTokenBody
+
+$TokenResponse = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody
 
 
+# Create header
+$Header = @{
+    Authorization = "$($token.token_type) $($token.access_token)"
+}
+#get all syncronization profiles
+$Uri = "https://graph.microsoft.com/beta/education/synchronizationProfiles"
+$SyncProfiles = Invoke-RestMethod -Uri $Uri -Headers $Header -Method Get -ContentType "application/json"
+$SyncProfiles.Value
 
-#domain and license for students&teachers 
+#####create synchronization profiles####
+#Getting SKuid
+
+$Uri12 = "https://graph.microsoft.com/v1.0/subscribedSkus"
+$start1 = Invoke-RestMethod -Uri $Uri12 -Headers $Header -Method get -ContentType "application/json"
+$skuid = $start1.value | select skuId, skupartNumber
+
+$studentskuIds = $skuid | where {($_.skuPartNumber -eq 'M365EDU_A5_STUDENT')}
+$studentskuIds.skuId
+$teacherskuIds = $skuid | where {($_.skuPartNumber -eq 'M365EDU_A5_FACULTY')}
+$teacherskuIds.skuId
+
+
 $studomain = "M365EDU032767.onmicrosoft.com"
 $teacherdomain = "M365EDU032767.onmicrosoft.com"
-$studentlicense = '46c119d4-0379-4a9d-85e4-97c66d3f909e'
-$teacherlicense = 'e97c048c-37a4-45fb-ab50-922fbf07a370'
+$studentlicense = $studentskuIds.skuId
+$teacherlicense = $teacherskuIds.skuId
 
-#create synchronization profile
-$apiUrl = 'https://graph.microsoft.com/beta/education/synchronizationProfiles'
 $body = 
 {
-    "displayName": "NewProfile12",
-    "dataProvider": {
-        "@odata.type": "#Microsoft.Education.DataSync.educationCsvDataProvider",
+    displayName = 'NewProfile12',
+    dataProvider = {
+        "@odata.type" : '#Microsoft.Education.DataSync.educationCsvDataProvider',
         "customizations": {
             "student": {
                 "optionalPropertiesToSync": [
@@ -74,15 +113,15 @@ $body =
         }
     },
     "identitySynchronizationConfiguration": {
-        "@odata.type": "#Microsoft.Education.DataSync.educationIdentityCreationConfiguration",
+        "@odata.type" : "#Microsoft.Education.DataSync.educationIdentityCreationConfiguration",
         "userDomains": [
             {
                 "appliesTo": "student",
-                "name": "$studomain"
+                "name": "M365EDU032767.onmicrosoft.com"
             },
             {
                 "appliesTo": "teacher",
-                "name": "$teacherdomain"
+                "name": "M365EDU032767.onmicrosoft.com"
             }
         ]
     },
@@ -90,33 +129,35 @@ $body =
         {
             "appliesTo": "teacher",
             "skuIds": [
-                "$teacherlicense"
+                "e97c048c-37a4-45fb-ab50-922fbf07a370"
             ]
         },
         {
             "appliesTo": "student",
             "skuIds": [
-                "$studentlicense"
+                "46c119d4-0379-4a9d-85e4-97c66d3f909e"
             ]
         }
     ]
 }
-Invoke-RestMethod -Headers @{Authorization = "Bearer $($TokenResponse.access_token)"} -Uri $apiUrl -Body $body -Method Post -ContentType 'application/json'
+$createdprofile = Invoke-RestMethod -Headers $Header -Uri $Uri -Body $body -Method Post -ContentType 'application/json'
 
-#get synchronization profile
-$apiUrl = 'https://graph.microsoft.com/beta/education/synchronizationProfiles'
-Invoke-RestMethod -Headers @{Authorization = "Bearer $($TokenResponse.access_token)"} -Uri $apiUrl -Method get -ContentType 'application/json'
+#upload url
+$Uri1 = "https://graph.microsoft.com/beta/education/synchronizationProfiles/f7ae02dd-de69-415c-bf24-9b8f22af6d9b/uploadUrl"
+$uploadurl = Invoke-RestMethod -Uri $Uri1 -Headers $Header -Method Get -ContentType "application/json"
 
-#create uploadurl
-$apiUrl = 'https://graph.microsoft.com/beta/education/synchronizationProfiles'
-$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method get
-$a = $Data | select value
+$b = $uploadurl.value
+$a = 'C:\azcopy\azcopy.exe copy "C:\local\*.*" '
+$c = ' --recursive=true --check-length=false'
 
-#create batch file using sas token value
+$u = "$a" + "'$b'" + "$c"
+$u >sastoken.cmd
 
 #run azcopy file #upload files using azcopy
-start-process -FilePath C:\Users\v-karapa\Desktop\SDS_UPLOAD_API\sastoken.cmd
+start-process -FilePath sastoken.cmd
 
-#start sync
-$apiUrl = 'https://graph.microsoft.com/beta/education/synchronizationProfiles/$syncprofile/start'
-Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method post -ContentType 'application/json'
+#Run start sync profile
+$UriStart = "https://graph.microsoft.com/beta/education/synchronizationProfiles/2f7c47a3-866e-4510-a926-70d7434b2b18/start"
+$start = Invoke-RestMethod -Uri $UriStart -Headers $Header -Method Post -ContentType "application/json"
+$start
+
